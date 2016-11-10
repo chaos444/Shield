fbe06a53d83b77c527df9e1b782a27b7482109b6
KenHH <kennth_5@hotmail.com>
2016-11-10 19:23:25 -0400

    Google formatting standard
    google formatting standard.

--- Shield.cpp
+++ Shield.cpp
@@ -9,251 +9,226 @@
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
-#define SAFE_RELEASE(P) if(P){P->Release() ; P = NULL ;}
-
+#define SAFE_RELEASE(P) \
+  if (P) {              \
+    P->Release();       \
+    P = NULL;           \
+  }
 
 BEGIN_MESSAGE_MAP(COverx64App, CWinApp)
-	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
 END_MESSAGE_MAP()
 
+CHackProcess HackProcess;
 
- CHackProcess HackProcess;
-
-COverx64App::COverx64App()
-{
-	m_dwRestartManagerSupportFlags = AFX_RESTART_MANAGER_SUPPORT_RESTART;
-
+COverx64App::COverx64App() {
+  m_dwRestartManagerSupportFlags = AFX_RESTART_MANAGER_SUPPORT_RESTART;
 }
 COverx64App theApp;
 
-PCSTR   MyWindowName = " nenene";
-HWND   MyHwnd;
+PCSTR MyWindowName = " nenene";
+HWND MyHwnd;
 
-PCSTR    GameWindowName = "ÊØÍûÏÈ·æ";
-PCSTR    GamePorcessName = "Overwatch.exe";
-HWND   GameHwnd;
-RECT      GameRectSize;
+PCSTR GameWindowName = "ï¿½ï¿½ï¿½ï¿½ï¿½È·ï¿½";
+PCSTR GamePorcessName = "Overwatch.exe";
+HWND GameHwnd;
+RECT GameRectSize;
 PROCESSENTRY32 GamePorcess;
-bool GameWindowMinimize=false;
+bool GameWindowMinimize = false;
 bool GameWindowResize = false;
-extern GameOffsets									_GameOffsets;
-extern CHackProcess									HackProcess;
+extern GameOffsets _GameOffsets;
+extern CHackProcess HackProcess;
 MyRectangle rectangle;
 
 char zhuchengxu[64];
 char tou[64];
 char wei[64];
 DWORD ID;
-INT Read_INT(INT64 address	, DWORD ½ø³ÌID)
-{
-	HANDLE ÄÚ´æ²Ù×÷¾ä±ú;
-	BOOL A;
-	INT ReadValue;
-	if (½ø³ÌID == -1)
-	{
-		ÄÚ´æ²Ù×÷¾ä±ú = GetCurrentProcess();
-	}
-	else
-	{
-		ÄÚ´æ²Ù×÷¾ä±ú = OpenProcess(2035711, NULL, ½ø³ÌID);
-	}
-	A = ReadProcessMemory(ÄÚ´æ²Ù×÷¾ä±ú, (LPVOID)address, &ReadValue, 4, NULL);
-	CloseHandle(ÄÚ´æ²Ù×÷¾ä±ú);
-	if (A == FALSE) { return INT(-1); }
-	else { return ReadValue; }
+INT Read_INT(INT64 address, DWORD ï¿½ï¿½ï¿½ï¿½ID) {
+  HANDLE ï¿½Ú´ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½;
+  BOOL A;
+  INT ReadValue;
+  if (ï¿½ï¿½ï¿½ï¿½ID == -1) {
+    ï¿½Ú´ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ = GetCurrentProcess();
+  } else {
+    ï¿½Ú´ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ = OpenProcess(2035711, NULL, ï¿½ï¿½ï¿½ï¿½ID);
+  }
+  A = ReadProcessMemory(ï¿½Ú´ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½, (LPVOID)address, &ReadValue, 4, NULL);
+  CloseHandle(ï¿½Ú´ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½);
+  if (A == FALSE) {
+    return INT(-1);
+  } else {
+    return ReadValue;
+  }
 }
-BOOL COverx64App::InitInstance()
-{
+BOOL COverx64App::InitInstance() {
+  CWinApp::InitInstance();
+  AfxEnableControlContainer();
+  CShellManager *pShellManager = new CShellManager;
+  CMFCVisualManager::SetDefaultManager(RUNTIME_CLASS(CMFCVisualManagerWindows));
 
-	CWinApp::InitInstance();
-	AfxEnableControlContainer();
-	CShellManager *pShellManager = new CShellManager;
-	CMFCVisualManager::SetDefaultManager(RUNTIME_CLASS(CMFCVisualManagerWindows));
+  MyCreateWindow();
 
- 			MyCreateWindow();
-
-	return TRUE;
-	
+  return TRUE;
 }
 
-LRESULT CALLBACK WinProc(HWND hWnd, UINT Message, WPARAM wParam, LPARAM lParam)
-{
-	switch (Message)
-	{
-	case WM_PAINT:
- 		DirectXInit();
-		DirectXRender();
-		break;
-	case WM_DESTROY:
-		CleanupDX();
-		PostQuitMessage(1);
-		return 0;
-	default:
-		return DefWindowProc(hWnd, Message, wParam, lParam);
-		break;
-	}
-	return 0;
+LRESULT CALLBACK WinProc(HWND hWnd, UINT Message, WPARAM wParam,
+                         LPARAM lParam) {
+  switch (Message) {
+    case WM_PAINT:
+      DirectXInit();
+      DirectXRender();
+      break;
+    case WM_DESTROY:
+      CleanupDX();
+      PostQuitMessage(1);
+      return 0;
+    default:
+      return DefWindowProc(hWnd, Message, wParam, lParam);
+      break;
+  }
+  return 0;
 }
-void COverx64App::MyCreateWindow()
-{
-	::CreateThread(0, 0, (LPTHREAD_START_ROUTINE)GetGameWindowRect, 0, 0, 0);
-		WNDCLASSEX wndClass;
-		wndClass.cbClsExtra = NULL;
-		wndClass.cbSize = sizeof(WNDCLASSEX);
-		wndClass.cbWndExtra = NULL;
-		wndClass.hbrBackground = (HBRUSH)CreateSolidBrush(RGB(0, 0, 0));
-		wndClass.hCursor = ::LoadCursor(0, IDC_ARROW);
-		wndClass.hIcon = ::LoadIcon(0, IDI_APPLICATION);
-		wndClass.hIconSm = ::LoadIcon(0, IDI_APPLICATION);
-		wndClass.hInstance = theApp.m_hInstance;
-		wndClass.lpfnWndProc = WinProc;
-		wndClass.lpszClassName = MyWindowName;
-		wndClass.lpszMenuName = MyWindowName;
-		wndClass.style = CS_VREDRAW | CS_HREDRAW;
+void COverx64App::MyCreateWindow() {
+  ::CreateThread(0, 0, (LPTHREAD_START_ROUTINE)GetGameWindowRect, 0, 0, 0);
+  WNDCLASSEX wndClass;
+  wndClass.cbClsExtra = NULL;
+  wndClass.cbSize = sizeof(WNDCLASSEX);
+  wndClass.cbWndExtra = NULL;
+  wndClass.hbrBackground = (HBRUSH)CreateSolidBrush(RGB(0, 0, 0));
+  wndClass.hCursor = ::LoadCursor(0, IDC_ARROW);
+  wndClass.hIcon = ::LoadIcon(0, IDI_APPLICATION);
+  wndClass.hIconSm = ::LoadIcon(0, IDI_APPLICATION);
+  wndClass.hInstance = theApp.m_hInstance;
+  wndClass.lpfnWndProc = WinProc;
+  wndClass.lpszClassName = MyWindowName;
+  wndClass.lpszMenuName = MyWindowName;
+  wndClass.style = CS_VREDRAW | CS_HREDRAW;
 
-	if (!RegisterClassEx(&wndClass))	exit(1);
+  if (!RegisterClassEx(&wndClass)) exit(1);
 
-	GameHwnd = FindWindow(0, GameWindowName);
-	if (GameHwnd)
-	{
-			GetWindowRect(GameHwnd, &GameRectSize);
-			MyHwnd = CreateWindowEx(
-			WS_EX_TOPMOST | 
-			WS_EX_TRANSPARENT | 
-			WS_EX_LAYERED|WS_EX_TOOLWINDOW,
-			MyWindowName,
-			MyWindowName,
-			WS_POPUP,
-			1, 1, 1920, 1080, 0, 0, 0, 0);
-			SetLayeredWindowAttributes(MyHwnd, 0, 1, LWA_ALPHA);
-			SetLayeredWindowAttributes(MyHwnd, 0, RGB(0, 0, 0), LWA_COLORKEY);
-			ShowWindow(MyHwnd, SW_SHOW);
-	}
-	MSG Message;
-	ZeroMemory(&Message, sizeof(Message));
-	while (GetMessage(&Message, NULL, 0, 0))
-	{
-		TranslateMessage(&Message);
-		DispatchMessage(&Message);
-		Sleep(1);
-	}
-
+  GameHwnd = FindWindow(0, GameWindowName);
+  if (GameHwnd) {
+    GetWindowRect(GameHwnd, &GameRectSize);
+    MyHwnd = CreateWindowEx(
+        WS_EX_TOPMOST | WS_EX_TRANSPARENT | WS_EX_LAYERED | WS_EX_TOOLWINDOW,
+        MyWindowName, MyWindowName, WS_POPUP, 1, 1, 1920, 1080, 0, 0, 0, 0);
+    SetLayeredWindowAttributes(MyHwnd, 0, 1, LWA_ALPHA);
+    SetLayeredWindowAttributes(MyHwnd, 0, RGB(0, 0, 0), LWA_COLORKEY);
+    ShowWindow(MyHwnd, SW_SHOW);
+  }
+  MSG Message;
+  ZeroMemory(&Message, sizeof(Message));
+  while (GetMessage(&Message, NULL, 0, 0)) {
+    TranslateMessage(&Message);
+    DispatchMessage(&Message);
+    Sleep(1);
+  }
 }
-extern GameDataWork	_GameData;
+extern GameDataWork _GameData;
 
-extern MyMenu							MyMenuA;
-extern MyChack							Buttn_ESP;
-extern MyChack							Buttn_Health;
-extern MyChack							Buttn_Distance;
-extern MyChack							Buttn_AimBot;
-extern MyChack							Buttn_AimBotPre;
-extern MyChack							Buttn_AimBotSpeed;
-extern MyChack							Buttn_Team;
-extern MyHealthButton					HealthStyle;
-extern MyAimBotHeight					AimBotHeight;
-extern MyAimBotKey						AimBotKey;
-extern MyAimBotSpeed					Buttn_AimBotSpeedInt;
-extern MyChangeButton					AimBotPre;
-extern MyChangeButton					AimBotPreHealth;
-extern MyChack							Buttn_AimBotPre;
-extern MyChack							Buttn_AimBotPreHealth;
+extern MyMenu MyMenuA;
+extern MyChack Buttn_ESP;
+extern MyChack Buttn_Health;
+extern MyChack Buttn_Distance;
+extern MyChack Buttn_AimBot;
+extern MyChack Buttn_AimBotPre;
+extern MyChack Buttn_AimBotSpeed;
+extern MyChack Buttn_Team;
+extern MyHealthButton HealthStyle;
+extern MyAimBotHeight AimBotHeight;
+extern MyAimBotKey AimBotKey;
+extern MyAimBotSpeed Buttn_AimBotSpeedInt;
+extern MyChangeButton AimBotPre;
+extern MyChangeButton AimBotPreHealth;
+extern MyChack Buttn_AimBotPre;
+extern MyChack Buttn_AimBotPreHealth;
 
-void  GetGameWindowRect()
-{
-	
-	while (true)
-	{
-		
-		GameHwnd = NULL;
-		GameHwnd = FindWindow(0, GameWindowName);
-		
-		if (GameHwnd == 0)
-			exit(0);
+void GetGameWindowRect() {
+  while (true) {
+    GameHwnd = NULL;
+    GameHwnd = FindWindow(0, GameWindowName);
 
+    if (GameHwnd == 0) exit(0);
 
-		if (GetKeyState(VK_INSERT) & 0x8000)
-		{
-			MyMenuA.MyMenuHideShow = !MyMenuA.MyMenuHideShow;
-			Sleep(100);
-		}
-		if (MyMenuA.MyMenuHideShow)
-		{
-			MyMenuA.MyClickPoint(GetMousePoint());
-			HealthStyle.MyButtonClick(GetMousePoint());
-			AimBotHeight.MyButtonClick(GetMousePoint());
-			AimBotKey.MyButtonClick(GetMousePoint());
-			Buttn_AimBotSpeed.MyChackClick(GetMousePoint());
-			Buttn_AimBotPre.MyChackClick(GetMousePoint());
-			Buttn_AimBotPreHealth.MyChackClick(GetMousePoint());
+    if (GetKeyState(VK_INSERT) & 0x8000) {
+      MyMenuA.MyMenuHideShow = !MyMenuA.MyMenuHideShow;
+      Sleep(100);
+    }
+    if (MyMenuA.MyMenuHideShow) {
+      MyMenuA.MyClickPoint(GetMousePoint());
+      HealthStyle.MyButtonClick(GetMousePoint());
+      AimBotHeight.MyButtonClick(GetMousePoint());
+      AimBotKey.MyButtonClick(GetMousePoint());
+      Buttn_AimBotSpeed.MyChackClick(GetMousePoint());
+      Buttn_AimBotPre.MyChackClick(GetMousePoint());
+      Buttn_AimBotPreHealth.MyChackClick(GetMousePoint());
 
-			Buttn_ESP.MyChackClick(GetMousePoint());
-			Buttn_Health.MyChackClick(GetMousePoint());
-			Buttn_Distance.MyChackClick(GetMousePoint());
-			Buttn_AimBot.MyChackClick(GetMousePoint());
-			Buttn_AimBotPre.MyChackClick(GetMousePoint());
-			Buttn_Team.MyChackClick(GetMousePoint());
-			Buttn_AimBotSpeedInt.MyButtonClick(GetMousePoint());
-			AimBotPreHealth.MyButtonClick(GetMousePoint());
-			AimBotPre.MyButtonClick(GetMousePoint());
-		}
-	
-		
-		if (GameHwnd)
-		{
-			if (!_GameData.GameProcessID)
-			{
-				_GameData.GameProcessID = HackProcess.GetProcess((char*)GamePorcessName, &GamePorcess);
-				_GameData.GameModuleOffset = HackProcess.GetProcessModuleOffset();
-			}
-	
-			RECT targetSize =RECT();
-			GetWindowRect(GameHwnd, &targetSize);
-			if (targetSize.left <= 0 && targetSize.top <= 0 && targetSize.right <= 0 && targetSize.bottom <= 0)
-			{
-				GameWindowMinimize = true;
-				continue;
-			}
-			GameWindowMinimize = false;
+      Buttn_ESP.MyChackClick(GetMousePoint());
+      Buttn_Health.MyChackClick(GetMousePoint());
+      Buttn_Distance.MyChackClick(GetMousePoint());
+      Buttn_AimBot.MyChackClick(GetMousePoint());
+      Buttn_AimBotPre.MyChackClick(GetMousePoint());
+      Buttn_Team.MyChackClick(GetMousePoint());
+      Buttn_AimBotSpeedInt.MyButtonClick(GetMousePoint());
+      AimBotPreHealth.MyButtonClick(GetMousePoint());
+      AimBotPre.MyButtonClick(GetMousePoint());
+    }
 
-			RECT borderSize = RECT();
-			GetClientRect(GameHwnd, &borderSize);
-			DWORD dwStyle = GetWindowLong(GameHwnd, GWL_STYLE);
-			int windowheight;
-			int windowwidth;
-			int borderheight;
-			int borderwidth;
+    if (GameHwnd) {
+      if (!_GameData.GameProcessID) {
+        _GameData.GameProcessID =
+            HackProcess.GetProcess((char *)GamePorcessName, &GamePorcess);
+        _GameData.GameModuleOffset = HackProcess.GetProcessModuleOffset();
+      }
 
-			if (rectangle.Width != (targetSize.bottom - targetSize.top)&& rectangle.Width != (borderSize.right - borderSize.left))
-				GameWindowResize = true;
+      RECT targetSize = RECT();
+      GetWindowRect(GameHwnd, &targetSize);
+      if (targetSize.left <= 0 && targetSize.top <= 0 &&
+          targetSize.right <= 0 && targetSize.bottom <= 0) {
+        GameWindowMinimize = true;
+        continue;
+      }
+      GameWindowMinimize = false;
 
-			rectangle.Width = targetSize.right - targetSize.left;
-			rectangle.Height = targetSize.bottom - targetSize.top;
-			rectangle.Left = targetSize.left;
-			rectangle.Top = targetSize.top;
-			_GameData.rect = rectangle;
-			if ((dwStyle & WS_BORDER) != 0)
-			{
-				windowheight = targetSize.bottom - targetSize.top;
-				windowwidth = targetSize.right - targetSize.left;
+      RECT borderSize = RECT();
+      GetClientRect(GameHwnd, &borderSize);
+      DWORD dwStyle = GetWindowLong(GameHwnd, GWL_STYLE);
+      int windowheight;
+      int windowwidth;
+      int borderheight;
+      int borderwidth;
 
-				rectangle.Height = borderSize.bottom - borderSize.top;
-				rectangle.Width = borderSize.right - borderSize.left;
+      if (rectangle.Width != (targetSize.bottom - targetSize.top) &&
+          rectangle.Width != (borderSize.right - borderSize.left))
+        GameWindowResize = true;
 
-				borderheight = (windowheight - borderSize.bottom);
-				borderwidth = (windowwidth - borderSize.right) / 2; /
-				borderheight -= borderwidth; //remove bottom
+      rectangle.Width = targetSize.right - targetSize.left;
+      rectangle.Height = targetSize.bottom - targetSize.top;
+      rectangle.Left = targetSize.left;
+      rectangle.Top = targetSize.top;
+      _GameData.rect = rectangle;
+      if ((dwStyle & WS_BORDER) != 0) {
+        windowheight = targetSize.bottom - targetSize.top;
+        windowwidth = targetSize.right - targetSize.left;
 
-				targetSize.left += borderwidth;
-				targetSize.top += borderheight;
+        rectangle.Height = borderSize.bottom - borderSize.top;
+        rectangle.Width = borderSize.right - borderSize.left;
 
-				rectangle.Left = targetSize.left;
-				rectangle.Top = targetSize.top;
-				_GameData.rect = rectangle;
-			}
-			MoveWindow(MyHwnd, targetSize.left, targetSize.top, rectangle.Width, rectangle.Height, true);
+        borderheight = (windowheight - borderSize.bottom);
+        borderwidth = (windowwidth - borderSize.right) / 2;
+        / borderheight -= borderwidth;  // remove bottom
 
-		}
-		Sleep(1);
-	}
-	
+        targetSize.left += borderwidth;
+        targetSize.top += borderheight;
+
+        rectangle.Left = targetSize.left;
+        rectangle.Top = targetSize.top;
+        _GameData.rect = rectangle;
+      }
+      MoveWindow(MyHwnd, targetSize.left, targetSize.top, rectangle.Width,
+                 rectangle.Height, true);
+    }
+    Sleep(1);
+  }
 }
